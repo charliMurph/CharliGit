98e9b2b7960a036e7ffae0909194d66af3e0e29b
Charli Murphy <n11245891@qut.edu.au>
2024-03-16 09:54:16 +1000

    Finished 2.4

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import obstacle.*;
+import obstacles.*;
 import common.*;
 
 
 class Main {
     public static void main(String[] args) {
+        // Parse the command line arguments into obstacles
+        // and create a map with those obstacles
         HashMap<String, ArrayList<String>> parsedArgs = parseArgs(args);
-        System.out.println(parsedArgs);
+        ArrayList<Obstacle> obstacles = parseObstacles(parsedArgs);
+        Map map = new Map(obstacles);
+
+        // Parse the start and target locations
+        String startArg = stripParentheses(parsedArgs.get("-start").get(0));
+        String targetArg = stripParentheses(parsedArgs.get("-target").get(0));
+        Location start = Location.parse(startArg);
+        Location target = Location.parse(targetArg);
+
+        // Show the map
+        System.out.println(map.getSolvedMap(start, target));
     }
 
     /**
