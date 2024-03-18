8c1f660a225b4e3c1273c4407f9cf23b39d8d135
Charli Murphy <162077969+charliMurph@users.noreply.github.com>
2024-03-18 10:10:12 +1000

    Added manually not through terminal

---
+++ src/obstacles/Obstacle.java
@@ -0,0 +1,17 @@
+package obstacles;
+
+public interface Obstacle {
+    /**
+     * Returns the symbol that represents the obstacle on the map
+     * @return The symbol that represents the obstacle on the map
+     */
+    char getSymbol();
+
+    /**
+     * Returns true if the given location is obstructed by the obstacle, and false otherwise
+     * @param x The x coordinate of the location
+     * @param y The y coordinate of the location
+     * @return True if the given location is obstructed by the obstacle, and false otherwise
+     */
+    boolean isLocationObstructed(int x, int y);
+}
